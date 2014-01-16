fcb381e7b5e560eb82ab0c86aed187ad871901e4
liiiidiiiia <liiiidiiiia@hotmail.com>
2014-01-17 00:16:55 +0100

    Update red_polje.h

--- red_polje.h
+++ red_polje.h
@@ -1,45 +1,43 @@
-#include <iostream>
-#include <cstring>
+#define N 1000
+#include<iostream>
 using namespace std;
 
-struct zapis{
-        string ime,prezime;
+struct tdatum{
+         int dan,mjesec, godina;
+};
+struct tpacijent{
+        char ime[8];
+        char prezime[8];
+        tdatum datum;
         char spol;
-        int d,m,g;
         int ai,bi,ci,di,ei;
 };
-
-struct queue{
-        zapis val[10000];
+struct tred{
+        tpacijent polje[N];
         int front,rear;
 };
+typedef int element;
+typedef tred queue;
 
-int AddOne(int n){
-        return ((n+1)%10000);
+int AddOne(int n) { 
+return((n+1)%1000); 
 }
 
-queue *InitQ(queue *Q){
-        Q = new queue;
-        Q->front = 0;
-        Q->rear = 9999;
-        return Q;
+tpacijent frontQ(queue *Q){
+        return Q->polje[Q->front];
 }
 
+void enqueueQ(tpacijent x, queue *Q){
+        Q->rear=AddOne(Q->rear);
+        Q->polje[Q->rear]=x;
+}
+void dequeueQ(queue *Q){
+        Q->front=AddOne(Q->front);
+}
+void initQ(queue *Q){
+        Q->front=0;
+        Q->rear=N-1;
+}
 bool IsEmptyQ(queue *Q){
-        if(AddOne(Q->rear)==Q->front) return true;
-        return false;
+        return (AddOne(Q->rear)==Q->front)? 1:0;
 }
-
-zapis FrontQ(queue *Q){
-        return Q->val[Q->front];
-}
-
-void EnQueueQ(zapis x,queue *Q){
-        Q->rear = AddOne(Q->rear);
-        Q->val[Q->rear] = x;
-}
-
-void DeQueueQ(queue *Q){
-        Q->front = AddOne(Q->front);
-}
- 
